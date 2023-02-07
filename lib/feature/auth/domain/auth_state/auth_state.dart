fc4c207fb09340f057e58a843f405972d00981cd
Andrey Martynov <martinov-studio@yandex.ru>
2023-02-07 16:30:50 +0300

    Created AuthCubit to manage authorization

---
+++ lib/feature/auth/domain/auth_state/auth_state.dart
@@ -0,0 +1,9 @@
+part of 'auth_cubit.dart';
+
+@freezed
+class AuthState with _$AuthState {
+  factory AuthState.unauthorized() = _AuthStateUnauthorized;
+  factory AuthState.authorized(UserEntity userEntity) = _AuthStateAuthorized;
+  factory AuthState.loading() = _AuthStateLoading;
+  factory AuthState.error(dynamic error) = _AuthStateError;
+}
