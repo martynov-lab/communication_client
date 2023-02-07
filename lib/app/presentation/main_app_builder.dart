7b78148ef1bbfaa6216f5779806e54bc867318e7
Andrey Martynov <martinov-studio@yandex.ru>
2023-02-07 21:10:33 +0300

    Initialize AuthCubit, added Global Provider

--- lib/app/presentation/main_app_builder.dart
+++ lib/app/presentation/main_app_builder.dart
@@ -1,14 +1,36 @@
+import 'package:communication_client/app/di/init_di.dart';
 import 'package:communication_client/app/domain/app_builder.dart';
+import 'package:communication_client/feature/auth/domain/auth_repository.dart';
+import 'package:communication_client/feature/auth/domain/auth_state/auth_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class MainAppBuilder implements AppBuilder {
   @override
   Widget buildApp() {
-    return const MaterialApp(
-      home: Scaffold(
-          body: Center(
-        child: Text("Hello"),
-      )),
+    return const _GlobalProviders(
+      child: MaterialApp(
+        home: Scaffold(
+            body: Center(
+          child: Text("Hello"),
+        )),
+      ),
+    );
+  }
+}
+
+class _GlobalProviders extends StatelessWidget {
+  final Widget child;
+  const _GlobalProviders({Key? key, required this.child}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+            create: ((context) => AuthCubit(locator<AuthRepository>())))
+      ],
+      child: child,
     );
   }
 }
