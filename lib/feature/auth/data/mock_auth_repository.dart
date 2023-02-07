7976dd706b50cef3ea99d79145ef405cf9a43853
Andrey Martynov <martinov-studio@yandex.ru>
2023-02-07 09:08:41 +0300

    Created and implemented mock implementation of MockAuthRepository

---
+++ lib/feature/auth/data/mock_auth_repository.dart
@@ -0,0 +1,61 @@
+import 'package:communication_client/feature/auth/domain/auth_repository.dart';
+import 'package:communication_client/feature/auth/domain/entities/user_entity/user_entity.dart';
+import 'package:injectable/injectable.dart';
+
+@Injectable(as: AuthRepository)
+@test
+class MockAuthRepository implements AuthRepository {
+  @override
+  Future getProfile() {
+    // TODO: implement getProfile
+    throw UnimplementedError();
+  }
+
+  @override
+  Future refrechToken({required String refreshToken}) {
+    // TODO: implement refrechToken
+    throw UnimplementedError();
+  }
+
+  @override
+  Future signIn({
+    required String username,
+    required String password,
+  }) {
+    return Future.delayed(const Duration(seconds: 2), () {
+      return UserEntity(
+        id: '-1',
+        email: 'testEmail@test.ru ',
+        userName: username,
+      );
+    });
+  }
+
+  @override
+  Future signUp({
+    required String username,
+    required String email,
+    required String password,
+  }) {
+    return Future.delayed(const Duration(seconds: 2), () {
+      return UserEntity(
+        id: '-1',
+        email: email,
+        userName: username,
+      );
+    });
+  }
+
+  @override
+  Future updatePassword(
+      {required String oldPassword, required String newPassword}) {
+    // TODO: implement updatePassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future updateUser({String? username, String? email}) {
+    // TODO: implement updateUser
+    throw UnimplementedError();
+  }
+}
